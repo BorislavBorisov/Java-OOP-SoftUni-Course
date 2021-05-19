53693dccc73e239cbc42119aec3b17a90807afec
BorislavBorisov <84284341+BorislavBorisov@users.noreply.github.com>
2021-05-19 19:36:14 +0300

    Add files via upload
    Exam Prep

---
+++ RobotService/models/procedures/BaseProcedure.java
@@ -0,0 +1,37 @@
+package robotService.models.procedures;
+
+import robotService.models.procedures.interfaces.Procedure;
+import robotService.models.robots.interfaces.Robot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static robotService.common.ExceptionMessages.INSUFFICIENT_PROCEDURE_TIME;
+
+public abstract class BaseProcedure implements Procedure {
+    List<Robot> robots;
+
+    protected BaseProcedure() {
+        this.robots = new ArrayList<>();
+    }
+
+    @Override
+    public String history() {
+        StringBuilder out = new StringBuilder(this.getClass().getSimpleName());
+        out.append(System.lineSeparator());
+        for (Robot robot : robots) {
+            out.append(robot.toString()).append(System.lineSeparator());
+        }
+        return out.toString().trim();
+    }
+
+    @Override
+    public void doService(Robot robot, int procedureTime) {
+        if (procedureTime > robot.getProcedureTime()) {
+            throw new IllegalArgumentException(INSUFFICIENT_PROCEDURE_TIME);
+        }
+        int newProcedureTime = robot.getProcedureTime() - procedureTime;
+        robot.setProcedureTime(newProcedureTime);
+        this.robots.add(robot);
+    }
+}
